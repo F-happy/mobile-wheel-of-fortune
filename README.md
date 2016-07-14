5d396050e045eaf00769473629b47e0d55bcae6e
jonnyf <fuhuixiang123@vip.qq.com>
2016-07-14 17:08:47 +0800

    完成组件开发

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# mobile-wheel-of-fortune
-这是一个由原生 js 编写的移动端高性能转盘插件
+# mobile-rotate
+> 这是一个由原生 js 编写的移动端高性能转盘插件, 在移动端上面使用了requestAnimationFrame 请求动画帧来进行逻辑的实现。
+
+### 安装：
+```
+npm install --save-dev mobile-rotate
+```
+
+### 开始使用：
+```javascript
+import rotate from 'mobile-rotate';
+
+let warp = document.querySelector('.rotate-warp');
+
+rotate(warp).rotate({
+    angle: 0,				// 开始旋转的原点
+    animateTo: 3000,	// 转动的角度
+    duration: 3000,		// 转动的时间
+    callback: ()=>{		// 旋转结束后的回调函数
+        // 活动旋转结束后的角度
+        console.log(rotate(bg).getRotateAngle())
+    }
+});
+
+```
+
+## API
+
+### rotate([opt])
+
+* opt
+
+Type：object
+
+这是组件的配置信息，用来设置组件的初始化信息，参数的内容参考上面的 demo。
+
+### getRotateAngle()
+
+* return string
+
+Type：function
+
+返回当前组件包裹的元素的旋转角度。
+
+
+## 兼容性：
+这个组件是用在移动端上面的，所以不兼容 IE 浏览器，
+并且由于使用了 requestAnimationFrame 因此请在使用前确保代码中有对请求动画帧做适配，如果没有的话请参考下面的代码。
+
+```javascript
+(function (window) {
+    var lastTime = 0;
+    window.requestAnimationFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        function (callback) {
+            var currTime   = new Date().getTime(),
+                timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+                id         = setTimeout(function () {
+                    callback(currTime + timeToCall);
+                }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+})(window);
+```
---
+++ index.js
@@ -0,0 +1,70 @@
+/**
+ * 这是一个在移动端的原生高性能Rotate组件
+ * @authors jonnyf
+ * @date    2016-07-08
+ * @version 1.0
+ */
+
+module.exports = (eleNode) => {
+    var ele     = eleNode,
+        supportedCSS,
+        styles  = document.querySelector("head").style,
+        toCheck = "transformProperty WebkitTransform OTransform msTransform MozTransform".split(" ");
+    supportedCSS = toCheck.filter((item)=> {
+        return styles[item] !== undefined;
+    }).join('');
+
+    ele.style.opacity = 1;
+    ele.style.backfaceVisibility = 'hidden';
+
+    function _rotate(startAngle, animateTo, overTime, callback) {
+        var _endAngle = startAngle || 0,
+            timeTag   = Date.now(),
+            stop      = null;
+
+        function runAnim() {
+            _endAngle++;
+            if (parseInt(Date.now() - timeTag) >= overTime || (_endAngle >= animateTo)) {
+                window.cancelAnimationFrame(stop);
+                callback();
+                return null;
+            }
+            var _angle = _easing(Date.now() - timeTag, startAngle, animateTo - startAngle, overTime) + startAngle;
+            _animate((~~(_angle * 10)) / 10);
+            stop = window.requestAnimationFrame(runAnim);
+        }
+
+        stop = window.requestAnimationFrame(runAnim);
+    }
+
+    // 真正实际操作 DOM 运动的函数
+    function _animate(angle) {
+        return ele.style[supportedCSS] = "rotateZ(" + (angle % 360) + "deg)";
+    }
+
+    // 变速运动的曲线
+    function _easing(t, b, c, d) {
+        return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+    }
+
+    return {
+        rotate: function (opt) {
+            var angle, animateTo, duration, callback = function () {
+            };
+            if (typeof opt == "undefined") {
+                return;
+            } else if (typeof opt == "number") {
+                angle = opt;
+            } else {
+                angle = opt['angle'];
+                animateTo = opt['animateTo'];
+                duration = opt['duration'];
+                callback = opt['callback'];
+            }
+            _rotate(angle, animateTo, duration, callback);
+        },
+        getRotateAngle: function () {
+            return ele.style.transform.replace(/[^0-9\.]+/g, '')
+        }
+    }
+};
